ceb2defc4508d9f641e92d4632fdab1badaafeb0
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-09-13 20:48:00 -0300

    Add button to show additional cards

--- scripts.js
+++ scripts.js
@@ -1,43 +1,49 @@
 const arr = [
   {
     id: '1',
+    mobilestatus: '',
     image: './images/plceholderspeakerimg.png',
-    name: 'Name Person',
+    name: 'Name Person 1',
     jobs: 'Professional experience and positions',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque optio, hic natus commodi labore aliquid?',
   },
   {
     id: '2',
+    mobilestatus: '',
     image: './images/plceholderspeakerimg.png',
-    name: 'Name Person',
+    name: 'Name Person 2',
     jobs: 'Professional experience and positions',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque optio, hic natus commodi labore aliquid?',
   },
   {
     id: '3',
+    mobilestatus: 'hidden',
     image: './images/plceholderspeakerimg.png',
-    name: 'Name Person',
+    name: 'Name Person 3',
     jobs: 'Professional experience and positions',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque optio, hic natus commodi labore aliquid?',
   },
   {
     id: '4',
+    mobilestatus: 'hidden',
     image: './images/plceholderspeakerimg.png',
-    name: 'Name Person',
+    name: 'Name Person 4',
     jobs: 'Professional experience and positions',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque optio, hic natus commodi labore aliquid?',
   },
   {
     id: '5',
+    mobilestatus: 'hidden',
     image: './images/plceholderspeakerimg.png',
-    name: 'Name Person',
+    name: 'Name Person 5',
     jobs: 'Professional experience and positions',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque optio, hic natus commodi labore aliquid?',
   },
   {
     id: '6',
+    mobilestatus: 'hidden',
     image: './images/plceholderspeakerimg.png',
-    name: 'Name Person',
+    name: 'Name Person 6',
     jobs: '',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque optio, hic natus commodi labore aliquid?',
   },
@@ -46,7 +52,7 @@ const arr = [
 const cardholder = document.getElementById('speakercontainer');
 
 arr.forEach((item) => {
-  cardholder.innerHTML += `<div class="card">
+  cardholder.innerHTML += `<div class="card ${item.mobilestatus}">
   <img class="cardimage" src=${item.image}>
   <div class="cardtext">
       <h3 class="cardtitle">${item.name}</h3>
@@ -76,3 +82,34 @@ closer.addEventListener('click', menuClose);
 const opener = document.querySelector('#mobilemenubutton');
 
 opener.addEventListener('click', menuOpen);
+
+const hiddencards = document.querySelectorAll('.hidden');
+console.log(hiddencards);
+
+const more = document.querySelector('#morebutton');
+
+function moreCards() {
+  hiddencards.forEach((card) => {
+    if (card.style.display === 'none') {
+      card.style.display = 'flex';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+const arrow = document.querySelector('#arrow');
+console.log(arrow);
+
+function buttonChanger() {
+  if (more.innerHTML === 'MORE <i id="arrow" class="fa-solid fa-greater-than">') {
+    more.innerHTML = 'LESS <i id="arrow" class="fa-solid fa-greater-than">';
+    const transformvalue = 'rotate(270deg)';
+    arrow.style.transform = transformvalue;
+  }
+}
+
+more.addEventListener('click', () => {
+  moreCards();
+  buttonChanger();
+});
